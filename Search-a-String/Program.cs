acc06252e3d75170b6c870dcf93dc8635bd20a58
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-13 20:27:41 -0500

    Alice in Wonderland search word

---
+++ Search-a-String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Search_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AIW = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            string input;
+            string lowerAIW = AIW.ToLower(); //convert all to lowercase
+            string[] arrOfLowerAIW = lowerAIW.Split(' '); //split string on blanks
+
+            Console.WriteLine(AIW);
+            Console.WriteLine("\n Enter a word to be searched in above sentence.");
+            input = Console.ReadLine();
+
+            for (int i=0; i < arrOfLowerAIW.Length; i++)
+            {
+                if (input.ToLower() == arrOfLowerAIW[i])
+                {
+                    Console.WriteLine("true");
+                }
+            }
+
+        }
+    }
+}
