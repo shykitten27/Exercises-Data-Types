a89bc7baf7e0672cb4755181a6c7e5c20adab211
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-13 19:40:28 -0500

    Add project files.

---
+++ Exercises-Data-Types/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercises_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
