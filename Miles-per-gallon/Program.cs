b7168ba430b9bffc1630312c88fe5a251374d1b3
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-13 19:59:29 -0500

    miles per gallon

---
+++ Miles-per-gallon/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Miles_per_gallon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milesDriven;
+            double fuelConsumed;
+            double mpg;
+            string input;
+
+            Console.WriteLine("Enter the total miles driven.");
+            input = Console.ReadLine();
+            milesDriven = double.Parse(input);
+            Console.WriteLine("Enter the total amount of fuel consumed.");
+            input = Console.ReadLine();
+            fuelConsumed = double.Parse(input);
+            mpg = milesDriven / fuelConsumed;
+            Console.WriteLine("The miles per gallon is " + mpg + ".");
+
+        }
+    }
+}
