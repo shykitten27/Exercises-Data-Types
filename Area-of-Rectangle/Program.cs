d70fd2732e81ce6c3b75110893e8ac57ad2a035d
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-13 19:53:34 -0500

    Personalized HelloWorld Area of rectangle

---
+++ Area-of-Rectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Area_of_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rectangleLength;
+            double rectangleWidth;
+            double rectangleArea;
+            string input;
+
+            Console.WriteLine("Enter the length of rectangle.");
+            input = Console.ReadLine();
+            rectangleLength = double.Parse(input);
+            Console.WriteLine("Enter the width of rectangle.");
+            input = Console.ReadLine();
+            rectangleWidth = double.Parse(input);
+            rectangleArea = rectangleLength * rectangleWidth;
+            Console.WriteLine("The area of a rectangle " + rectangleLength + " in length and " + rectangleWidth + " in width is " + rectangleArea + ".");
+        }
+    }
+}
