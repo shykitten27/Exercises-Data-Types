c1ad7e5c927bee8bfd53fd1f996d6cdaf3983dc2
D Seidel <67442972+shykitten27@users.noreply.github.com>
2021-07-19 10:55:38 -0500

    use IndexOf instead of arrays

--- Search-a-String/Program.cs
+++ Search-a-String/Program.cs
@@ -6,23 +6,19 @@ namespace Search_a_String
     {
         static void Main(string[] args)
         {
-            string AIW = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
-            string input;
-            string lowerAIW = AIW.ToLower(); //convert all to lowercase
-            string[] arrOfLowerAIW = lowerAIW.Split(' '); //split string on blanks
+            string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            Console.WriteLine(alice);
+            Console.WriteLine("\nEnter a word to be searched in above sentence.");
+            string searchTerm = Console.ReadLine();
+            string completeSearchTerm = searchTerm.ToLower(); //convert all to lowercase
+            string completeAlice = alice.ToLower(); //convert all to lowercase
+    
+            int foundIndex = completeAlice.IndexOf(completeSearchTerm, 0);
+            Console.WriteLine("\nThe search term " + searchTerm + ", can be found at " + foundIndex + ".");
+            Console.WriteLine("\nThe search term is " + searchTerm.Length + " characters long.");
 
-            Console.WriteLine(AIW);
-            Console.WriteLine("\n Enter a word to be searched in above sentence.");
-            input = Console.ReadLine();
-
-            for (int i=0; i < arrOfLowerAIW.Length; i++)
-            {
-                if (input.ToLower() == arrOfLowerAIW[i])
-                {
-                    Console.WriteLine("true");
-                }
-            }
-
+            string newAlice = alice.Remove(foundIndex, searchTerm.Length);
+            Console.WriteLine(newAlice);
         }
     }
 }
